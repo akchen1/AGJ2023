59775c96ab0b4e2ba8262e36d5a164b74d5cae8e
akchen1 <43446512+akchen1@users.noreply.github.com>
2023-06-03 21:08:50 -0600

    add light it up minigame

---
+++ Assets/GameAssets/Code/Gameplay/Minigames/LightItUp/MatchContainer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MatchContainer : MonoBehaviour, IBeginDragHandler, IDragHandler
+{
+    [SerializeField] private GameObject matchPrefab;
+    [SerializeField] private Transform matchParent;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        GameObject match = Instantiate(matchPrefab, matchParent);
+        eventData.pointerDrag = match;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Required here to work
+    }
+}
