7e74a0d9679e5d4206690f412525ad57687c28b3
Gaibryol <gaibryol@gmail.com>
2023-06-03 19:54:35 -0600

    Added fade to black mechanic between scene 7

--- Assets/GameAssets/Code/Gameplay/SceneControllers/SearchScene7Controller.cs
+++ Assets/GameAssets/Code/Gameplay/SceneControllers/SearchScene7Controller.cs
@@ -3,11 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UI;
 using Cinemachine;
 
 public class SearchScene7Controller : SceneController
 {
 	[SerializeField] private PlayableDirector playableDirector;
+	[SerializeField] private Image fadeToBlack;
+	[SerializeField] private float transitionSpeedMultiplier;
+	[SerializeField] private float transitionTime;
 
 	[SerializeField, Header("Main Street")] CinemachineVirtualCamera mainStreetCam;
 	[SerializeField] private PlayableAsset mainStreetStartingCutscene;
@@ -28,6 +32,7 @@ public class SearchScene7Controller : SceneController
 	{
 		Constants.Scene7SubScenes subscene = inEvent.Payload.Subscene;
 
+		StartCoroutine(FadeBetweenCams());
 		HandleCameras(subscene);
 
 		switch (inEvent.Payload.Subscene)
@@ -62,10 +67,35 @@ public class SearchScene7Controller : SceneController
 		playgroundCam.enabled = subscene == Constants.Scene7SubScenes.Playground;
 	}
 
+	private IEnumerator FadeBetweenCams()
+	{
+		fadeToBlack.gameObject.SetActive(true);
+		fadeToBlack.color = new Color(fadeToBlack.color.r, fadeToBlack.color.g, fadeToBlack.color.b, 0);
+
+		while (fadeToBlack.color.a < 1f)
+		{
+			fadeToBlack.color = new Color(fadeToBlack.color.r, fadeToBlack.color.g, fadeToBlack.color.b, fadeToBlack.color.a + (Time.deltaTime * transitionSpeedMultiplier));
+			yield return null;
+		}
+
+		fadeToBlack.color = new Color(fadeToBlack.color.r, fadeToBlack.color.g, fadeToBlack.color.b, 1f);
+
+		yield return new WaitForSeconds(transitionTime);
+
+		while (fadeToBlack.color.a > 0f)
+		{
+			fadeToBlack.color = new Color(fadeToBlack.color.r, fadeToBlack.color.g, fadeToBlack.color.b, fadeToBlack.color.a - (Time.deltaTime * transitionSpeedMultiplier));
+			yield return null;
+		}
+
+		fadeToBlack.color = new Color(fadeToBlack.color.r, fadeToBlack.color.g, fadeToBlack.color.b, 0f);
+		fadeToBlack.gameObject.SetActive(false);
+	}
+
 	private void Start()
 	{
 		HandleCameras(Constants.Scene7SubScenes.MainStreet);
-
+		fadeToBlack.gameObject.SetActive(false);
 		playableDirector?.Play(mainStreetStartingCutscene);
 
 	}
