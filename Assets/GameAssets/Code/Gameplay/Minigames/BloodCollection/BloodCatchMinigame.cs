2d4322fad13bacd16e0d5f032ae4d5aace9457c7
Gaibryol <gaibryol@gmail.com>
2023-06-05 21:53:58 -0600

    Finihsed blood minigame

---
+++ Assets/GameAssets/Code/Gameplay/Minigames/BloodCollection/BloodCatchMinigame.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BloodCatchMinigame : MonoBehaviour, IMinigame
+{
+	private EventBrokerComponent eventBrokerComponent = new EventBrokerComponent();
+
+	[SerializeField] private List<InventoryItem> neededItems;
+	[SerializeField] private int bloodNeeded;
+
+	[SerializeField, Header("UI")] private GameObject panel;
+
+	[SerializeField, Header("Vial")] private GameObject vial;
+
+	[SerializeField, Header("Blood")] private GameObject bloodPrefab;
+	[SerializeField] private Vector2 minBloodSpawnPos;
+	[SerializeField] private Vector2 maxBloodSpawnPos;
+	[SerializeField] private float bloodSpawnTimer;
+	[SerializeField] private float bloodSpawnTimerOffset;
+	[SerializeField] private float bloodFallSpeed;
+	[SerializeField] private Transform bloodHolder;
+
+	private bool gameRunning = false;
+	private int totalBlood;
+
+	private List<GameObject> bloodList = new List<GameObject>();
+
+	private float timer;
+
+	public void Finish()
+	{
+		foreach(GameObject blood in bloodList)
+		{
+			Destroy(blood);
+		}
+
+		gameRunning = false;
+		panel.SetActive(false);
+		eventBrokerComponent.Publish(this, new MinigameEvents.EndMinigame());
+	}
+
+	public void Initialize()
+	{
+		panel.SetActive(true);
+		timer = Random.Range(bloodSpawnTimer - bloodSpawnTimerOffset, bloodSpawnTimer + bloodSpawnTimerOffset);
+
+		totalBlood = 0;
+		gameRunning = true;
+	}
+
+	public bool StartCondition()
+	{
+		int numItems = 0;
+		foreach (InventoryItem item in neededItems)
+		{
+			eventBrokerComponent.Publish(this, new InventoryEvents.HasItem(item, callback =>
+			{
+				if (callback)
+				{
+					numItems += 1;
+				}
+			}));
+		}
+
+		return numItems == neededItems.Count;
+	}
+
+	private void SpawnBlood()
+	{
+		Vector3 spawnPos = new Vector3(Random.Range(minBloodSpawnPos.x, maxBloodSpawnPos.x), Random.Range(minBloodSpawnPos.y, maxBloodSpawnPos.y), 0f);
+		GameObject blood = Instantiate(bloodPrefab, spawnPos, Quaternion.identity);
+		blood.transform.SetParent(bloodHolder);
+		blood.GetComponent<BloodDrop>().Initialize(bloodFallSpeed, OnBloodCollected, OnBloodOutOfBounds);
+		bloodList.Add(blood);
+	}
+
+	private void OnBloodCollected(GameObject blood)
+	{
+		bloodList.Remove(blood);
+		Destroy(blood);
+
+		totalBlood += 1;
+	}
+
+	private void OnBloodOutOfBounds(GameObject blood)
+	{
+		bloodList.Remove(blood);
+		Destroy(blood);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (!gameRunning)
+		{
+			return;
+		}
+
+		if (totalBlood == bloodNeeded)
+		{
+			Finish();
+			return;
+		}
+
+        if (timer > 0)
+		{
+			timer -= Time.deltaTime;
+		}
+		else
+		{
+			timer = Random.Range(bloodSpawnTimer - bloodSpawnTimerOffset, bloodSpawnTimer + bloodSpawnTimerOffset);
+			SpawnBlood();
+		}
+
+		vial.transform.position = Input.mousePosition;
+    }
+}
