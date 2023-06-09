3e62b4971d4950cf9837fede34b49493ec70c556
BhanHG <96505078+BhanHG@users.noreply.github.com>
2023-06-09 03:08:25 -0600

    Updated
    Added Collider to scene 1-3
    Added Lock Combination Prefab.

---
+++ Assets/GameAssets/Code/Gameplay/Minigames/CombinationLock/ButtonScroll.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScroll : MonoBehaviour
+{
+    private ScrollRect scrollRect;
+    public float scrollSpeed = 200f;
+    public float scrollDistance = 50f;
+
+    private bool isScrolling = false;
+    private Vector2 targetScrollPosition;
+
+    private int direction = 0;
+    public int ID;
+    private EventBrokerComponent eventBrokerComponent = new EventBrokerComponent();
+
+    void Start()
+    {
+        // Set the initial scroll position
+        scrollRect = GetComponent<ScrollRect>();
+        targetScrollPosition = scrollRect.content.anchoredPosition;
+    }
+
+    void Update()
+    {
+        if (isScrolling)
+        {
+            // Smoothly scroll towards the target position
+            scrollRect.content.anchoredPosition = Vector2.MoveTowards(scrollRect.content.anchoredPosition, targetScrollPosition, scrollSpeed * Time.deltaTime);
+
+            // Check if we have reached the target position
+            if (Vector2.Distance(scrollRect.content.anchoredPosition, targetScrollPosition) < 0.01f)
+            {
+                if(direction == 1)
+                {
+                    RectTransform firstChild = transform.GetChild(9).GetComponent<RectTransform>();
+
+                    // Move the first child to the bottom of the hierarchy
+                    firstChild.SetAsFirstSibling();
+
+                    // Adjust the content position to reflect the change
+                    Vector2 contentPosition = scrollRect.content.anchoredPosition;
+                    contentPosition.y += firstChild.rect.height;
+                    scrollRect.content.anchoredPosition = contentPosition;
+                }
+                else if(direction == -1)
+                {
+                    RectTransform firstChild = transform.GetChild(0).GetComponent<RectTransform>();
+
+                    // Move the first child to the bottom of the hierarchy
+                    firstChild.SetAsLastSibling();
+
+                    // Adjust the content position to reflect the change
+                    Vector2 contentPosition = scrollRect.content.anchoredPosition;
+                    contentPosition.y -= firstChild.rect.height;
+                    scrollRect.content.anchoredPosition = contentPosition;
+                }
+                transform.parent.parent.parent.GetComponent<LockCombination>().CheckCorrect();
+                isScrolling = false;
+            }
+        }
+    }
+
+    public void Increment()
+    {
+        if (!isScrolling)
+        {
+            // Calculate the new target position for scrolling down
+            Vector2 currentScrollPosition = scrollRect.content.anchoredPosition;
+            targetScrollPosition = currentScrollPosition + new Vector2(0f, -scrollDistance);
+            direction = 1;
+
+            // Start scrolling
+            isScrolling = true;
+        }
+    }
+
+    public void Decrement()
+    {
+        if (!isScrolling)
+        {
+            // Calculate the new target position for scrolling up
+            Vector2 currentScrollPosition = scrollRect.content.anchoredPosition;
+            targetScrollPosition = currentScrollPosition + new Vector2(0f, scrollDistance);
+            direction = -1;
+
+            // Start scrolling
+            isScrolling = true;
+        }
+    }
+
+
+    private void OnEnable() {
+        eventBrokerComponent.Subscribe<LockEvent.GetCombination>(GetCombinationHandler);
+    }
+    private void GetCombinationHandler(BrokerEvent<LockEvent.GetCombination> inEvent)
+    {
+        inEvent.Payload.ProcessData.DynamicInvoke(transform.GetChild(8).name, ID);
+    }
+    private void OnDisable()
+    {
+        eventBrokerComponent.Unsubscribe<LockEvent.GetCombination>(GetCombinationHandler);
+    }
+}
