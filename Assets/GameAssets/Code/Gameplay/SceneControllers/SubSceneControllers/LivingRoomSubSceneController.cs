55cf6713662f0bbf0318246209a2c94ef70d2c27
akchen1 <43446512+akchen1@users.noreply.github.com>
2023-07-08 03:37:09 -0600

    search scene living room updates

--- Assets/GameAssets/Code/Gameplay/SceneControllers/SubSceneControllers/LivingRoomSubSceneController.cs
+++ Assets/GameAssets/Code/Gameplay/SceneControllers/SubSceneControllers/LivingRoomSubSceneController.cs
@@ -1,20 +1,35 @@
+using DS.ScriptableObjects;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 [System.Serializable]
 public class LivingRoomSubSceneController : SubSceneController
 {
     [SerializeField] private InventoryItem pocketKnife;
-    [SerializeField] private DialogueInteraction onKnifeObtainedDialogue;
+    [SerializeField] private DSDialogueSO baduSanityDialogue;
+    [SerializeField] private DSDialogueSO sanityChoiceDialogueNode;
+    [SerializeField] private PlayableDirector director;
+    [SerializeField] private PlayableAsset maeveLeaveCutscene;
+    [SerializeField] private PlayableAsset vaseBreakCutscene;
 
+    [SerializeField] private Animator baduAnimator;
     private bool pocketKnifeObtained = false;
+    private bool isFirstInteract = true;
+    private bool sanityDialogueStarted = false;
     public override void Enable()
     {
         base.Enable();
         eventBrokerComponent.Subscribe<InventoryEvents.AddItem>(AddItemHandler);
         eventBrokerComponent.Subscribe<InteractionEvents.InteractEnd>(InteractEndHandler);
+        eventBrokerComponent.Subscribe<DialogueEvents.SelectDialogueOption>(SelectDialogueOptionHandler);
+        eventBrokerComponent.Subscribe<DialogueEvents.DialogueFinish>(DialogueFinishHandler);
+        
+        baduAnimator.SetTrigger(pocketKnifeObtained ? "fly" : "idle");
+
+        
     }
 
     public override void Disable()
@@ -22,6 +37,25 @@ public class LivingRoomSubSceneController : SubSceneController
         base.Disable();
         eventBrokerComponent.Unsubscribe<InventoryEvents.AddItem>(AddItemHandler);
         eventBrokerComponent.Unsubscribe<InteractionEvents.InteractEnd>(InteractEndHandler);
+        eventBrokerComponent.Unsubscribe<DialogueEvents.SelectDialogueOption>(SelectDialogueOptionHandler);
+        eventBrokerComponent.Unsubscribe<DialogueEvents.DialogueFinish>(DialogueFinishHandler);
+    }
+
+    private void DialogueFinishHandler(BrokerEvent<DialogueEvents.DialogueFinish> obj)
+    {
+        if (!sanityDialogueStarted) return;
+        baduAnimator.SetBool("isBadu", false);
+        baduAnimator.SetTrigger("fly");
+        sanityDialogueStarted = false;
+    }
+
+    private void SelectDialogueOptionHandler(BrokerEvent<DialogueEvents.SelectDialogueOption> obj)
+    {
+        if (!sanityDialogueStarted) return;
+        if (obj.Payload.Option == sanityChoiceDialogueNode.Choices[0])
+        {
+            director.Play(vaseBreakCutscene);
+        }
     }
 
     private void AddItemHandler(BrokerEvent<InventoryEvents.AddItem> obj)
@@ -37,10 +71,18 @@ public class LivingRoomSubSceneController : SubSceneController
 
     private void InteractEndHandler(BrokerEvent<InteractionEvents.InteractEnd> obj)
     {
-        if (pocketKnifeObtained)
+        if (pocketKnifeObtained && isFirstInteract)
         {
-            onKnifeObtainedDialogue.Interact();
-            pocketKnifeObtained = false;
+            director.Play(maeveLeaveCutscene);
+            director.stopped += OnMaeveLeaveCutsceneFinish;
+            isFirstInteract = false;
         }
+    }
+
+    private void OnMaeveLeaveCutsceneFinish(PlayableDirector obj)
+    {
+        baduSanityDialogue.Interact(this, Constants.Interaction.InteractionType.Virtual);
+        director.stopped -= OnMaeveLeaveCutsceneFinish;
+        sanityDialogueStarted = true;
     }
 }
