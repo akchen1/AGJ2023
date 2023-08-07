2069df5c312f8e6dabf810e5402695b1f8b6da7c
akchen1 <43446512+akchen1@users.noreply.github.com>
2023-08-07 16:13:40 -0600

    remove includes

--- Assets/GameAssets/Code/Systems/Sanity/SanitySystem.cs
+++ Assets/GameAssets/Code/Systems/Sanity/SanitySystem.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.Rendering.PostProcessing;
 using static Constants.Sanity;
 
